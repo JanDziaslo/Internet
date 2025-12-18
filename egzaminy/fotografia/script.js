0f3e26c2aef0ad419ae043d89d0c83b394641d73
JanDziaslo <u22_bartosznikitiuk@zsp1.siedlce.pl>
2025-12-18 11:50:10 +0100

    nie dokonczony  javascrript

---
+++ egzaminy/fotografia/script.js
@@ -0,0 +1,13 @@
+function skrypt()
+{
+    var kopie = document.getElementById("kopie").value
+    var blyszczacy = document.getElementById("blyszczacy").checked
+    var matowy = document.getElementById("matowy").checked
+    
+    console.log(kopie + " " + blyszczacy + " " + matowy)
+    
+    if (blyszczacy === true)
+    {
+        
+    }
+}
