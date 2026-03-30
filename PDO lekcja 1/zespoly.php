f3d9c8133951b8324a0156bbb773d97af8b0b483
JanDziaslo <u22_bartosznikitiuk@zsp1.siedlce.pl>
2026-03-30 13:58:12 +0200

    przyciski w tabeli

--- PDO lekcja 1/zespoly.php
+++ PDO lekcja 1/zespoly.php
@@ -11,7 +11,17 @@ require_once 'database.php';
     <title>Zespoly</title>
 </head>
 <body>
-
+<!-- edycja -->
+<svg xmlns="http://www.w3.org/2000/svg" class="d-none">
+    <symbol id="pencil" viewBox="0 0 16 16">
+        <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325"/>
+</svg>
+<!-- usuwanie -->
+<svg xmlns="http://www.w3.org/2000/svg" class="d-none">
+    <symbol id="smietnik" viewBox="0 0 16 16">
+        <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
+        <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
+</svg>
 <!-- blad -->
 <svg xmlns="http://www.w3.org/2000/svg" class="d-none">
     <symbol id="exclamation-triangle-fill" viewBox="0 0 16 16">
@@ -83,6 +93,7 @@ if(isset($_POST['submit']) && $_POST['search']!=''){
                     <th>ID Zespołu</th>
                     <th>Nazwa</th>
                     <th>Adres</th>
+                    <th>Akcje</th>
                 </tr>
                 </thead>
                 <tbody>
@@ -92,6 +103,8 @@ if(isset($_POST['submit']) && $_POST['search']!=''){
                     echo '<td>'.$row['ID_ZESP'].'</td>';
                     echo '<td>'.$row['NAZWA'].'</td>';
                     echo '<td>'.$row['ADRES'].'</td>';
+                    echo '<td><a href="edytuj_zesp.php?id='.$row['ID_ZESP'].'"><button type="button" class="btn btn-outline-secondary me-2"><svg width="16" height="16" fill="white"><use xlink:href="#pencil"></use></svg></button></a>';
+                    echo '<a href="zespoly.php?action=delete&idp='.$row['ID_ZESP'].'"><button type="button" class="btn btn-outline-secondary me-2"><svg width="16" height="16" fill="red"><use xlink:href="#smietnik"></use></svg></button></a>';
                     echo '</tr>';
                 }
                 ?>
